1e70b3b59bc531c204676e9f44a97cd8209e7cff
priz80 <priz8090@gmail.com>
2025-10-21 15:40:45 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 new WOW().init();
+/* const swiper = new Swiper('.swiper', {
+  speed: 400,
+  spaceBetween: 100,
+}); */
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
